5229f8c67f77fec753040f0a6407fe4575015157
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-12-15 20:23:05 +0000

    Add logic for creating a todo object

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Todo;
+use App\Http\Controllers\Controller;
 
 class TodoController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'details' => 'nullable|string',
+            'status' => 'in:complete,in progress,not started'
+        ]);
+
+        $todo = Todo::create($validated);
+
+        return response()->json($todo, 201);
+    }
 }
